d97391cd1a377930b2ac8091487bcc1315de9f20
nobugsatcoding <nobugsatcoding@gmail.com>
2024-03-29 15:25:00 +0200

    template created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contacts from './components/Contacts';
+import Gallery from './components/Gallery';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
+          <Route path="contacts" element={<Contacts />} />
+          <Route path="gallery" element={<Gallery />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
