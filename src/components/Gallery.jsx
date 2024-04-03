3001f4adba39f86c489a3f2cd14adfd83e70ace0
nobugsatcoding <nobugsatcoding@gmail.com>
2024-04-03 22:02:32 +0300

    gallery added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,36 @@
-
+import { useState } from 'react';
+import { galleryArray } from '../gallery/GalleryArray';
+import styles from './Gallery.module.css';
+import { TiChevronLeft } from 'react-icons/ti';
+import { TiChevronRight } from 'react-icons/ti';
 
 const Gallery = () => {
-  return (
-    <div>GALERIJA</div>
-  )
-}
+  const [currentID, setCurrentID] = useState(0);
 
-export default Gallery
+  const prevPicture = () => {
+    setCurrentID((currentID - 1 + galleryArray.length) % galleryArray.length);
+  };
+
+  const nextPicture = () => {
+    setCurrentID((currentID + 1) % galleryArray.length);
+  };
+
+  return (
+    <div className={styles.gallery}>
+      <div onClick={prevPicture} className={styles.icon}>
+        <TiChevronLeft />
+      </div>
+      <div>
+        <img
+          src={galleryArray[currentID].image}
+          alt={galleryArray[currentID].description}
+        />
+      </div>
+      <div onClick={nextPicture} className={styles.icon}>
+        <TiChevronRight />
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/gallery/GalleryArray.jsx
@@ -0,0 +1,20 @@
+export const galleryArray = [
+    {
+        id: 1, image: require('./gal_0.jpg'), description: "Projektavimas"
+    },
+    {
+        id: 2, image: require('./gal_1.jpeg'), description: "Instaliavimas"
+    },
+    {
+        id: 3, image: require('./gal_2.jpg'), description: "Montavimas"
+    },
+    {
+        id: 4, image: require('./gal_3.webp'), description: "Surinkimas"
+    },
+    {
+        id: 5, image: require('./gal_4.jpg'), description: "Planavimas"
+    },
+    {
+        id: 6, image: require('./gal_5.jpg'), description: "Kontrolė"
+    },
+];
